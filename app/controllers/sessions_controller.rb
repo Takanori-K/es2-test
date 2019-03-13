e5aa5f1871f3d1d58b8cd23d0e5b4d473a08c6f5
Kaku <kaku.takanori.1026@gmail.com>
2019-03-13 14:39:28 +0000

    login-flash-message

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      # 実装はまだ完了していません
+      flash.now[:danger] = 'メールアドレスとパスワードの情報が一致しませんでした。'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
