c268b185f73fbc159132abb70eed657affb1a8b2
Kaku <kaku.takanori.1026@gmail.com>
2019-03-19 08:54:13 +0000

    basic_time_work_time

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true 
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates :department, length: { in: 3..50 }, allow_blank: true
 end
